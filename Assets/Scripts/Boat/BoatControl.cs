c819fcf01f95bb03936c43d561a65b7aea472c86
Kai Eschmann <kai.eschmann@gmx.de>
2021-01-30 00:38:48 +0100

    Basic Orbit Cam + Boat Movement

---
+++ Assets/Scripts/Boat/BoatControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatControl : MonoBehaviour
+{
+
+    public float m_forwardVel;
+    public float m_turnVel;
+    public float ForwardVel
+    {
+        get => m_forwardVel;
+    }
+    
+    public float TurnVel
+    {
+        get => m_turnVel;
+    }
+
+    private Quaternion m_turnRotation;
+
+    public Quaternion TurnRotation
+    {
+        get => m_turnRotation;
+    }
+
+    [SerializeField]private float m_speedModifier;
+    [SerializeField] private float m_turnModifier;
+
+    public float m_maxVerticalSpeed;
+    public float m_maxTurnSpeed;
+    
+    public void AddSpeed(float speed)
+    {
+        
+        m_forwardVel += (speed * m_speedModifier);
+        m_forwardVel = Mathf.Clamp(m_forwardVel, -m_maxVerticalSpeed, m_maxVerticalSpeed);
+    }
+
+    public void AddTurnSpeed(float turnSpeed)
+    {
+        m_turnVel += (turnSpeed * m_turnModifier);
+        m_turnVel = Mathf.Clamp(m_turnVel, -m_maxTurnSpeed, m_maxTurnSpeed);
+        m_turnRotation = Quaternion.AngleAxis(m_turnVel * m_turnModifier, transform.up);
+    }
+}
---
+++ Assets/Scripts/Camera/OrbitalCamera.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class OrbitalCamera : MonoBehaviour
+{
+    public Transform m_target;
+
+
+    [SerializeField] private float m_armLength = 5f;
+
+    public Vector2 m_orbitAngles;
+
+    public float m_rotationSpeed = 90f;
+    
+    
+    void Start()
+    {
+        m_target = FindObjectOfType<BoatManager>().transform;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(m_target);
+        
+        RotateCamera();
+        Quaternion lookRot = Quaternion.Euler(m_orbitAngles);
+        Vector3 lookDir = lookRot * Vector3.forward;
+        Vector3 lookPos = m_target.position - lookDir * m_armLength;
+        transform.SetPositionAndRotation(lookPos, lookRot);
+    }
+
+    private void RotateCamera()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("HorCam"), Input.GetAxis("VertCam"));
+        if (input.sqrMagnitude > 0.1f)
+        {
+            m_orbitAngles += m_rotationSpeed * Time.fixedDeltaTime * input;
+        }
+    }
+}
