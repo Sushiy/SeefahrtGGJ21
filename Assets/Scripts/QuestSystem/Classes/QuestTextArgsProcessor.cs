b075c19336a7977957763abb8a4cac91b698b910
Sebastian Krause <sebastian@battyRabbit.com>
2021-01-30 18:41:26 +0100

    Parser und Quest-System verbessert

---
+++ Assets/Scripts/QuestSystem/Classes/QuestTextArgsProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Simple Replacement", menuName = "Quest Text Processors/Argument Parser")]
+public class QuestTextArgsProcessor : QuestTextProcessor
+{
+    [System.Serializable]
+    public struct FArgumentToString
+    {
+        public string Key;
+        public string Value;
+    }
+
+    public int ArgumentIndex = 0;
+    public bool KeyIsValue = false;
+    public FArgumentToString[] ArgumentToString;
+
+    bool HasKey(string Key, out string FoundArg)
+    {
+        if (KeyIsValue)
+        {
+            FoundArg = Key;
+            return true;
+        }
+        else
+        {
+            foreach (var argumentToString in ArgumentToString)
+            {
+                if (Key == argumentToString.Key)
+                {
+                    FoundArg = argumentToString.Value;
+                    return true;
+                }
+            }
+
+            FoundArg = null;
+            return false;
+        }
+    }
+
+    /// <inheritdoc />
+    public override string Process(string InputText, QuestCompletionParameters Params)
+    {
+        int TagFrom, TagTo;
+        string Arg = GetTagArgument(InputText, 0, out TagFrom, out TagTo);
+        string foundArg;
+
+        if (Arg != null && HasKey(Arg, out foundArg))
+        {
+            InputText = InputText.Remove(TagFrom, TagTo - TagFrom + 1);
+            return InputText.Insert(TagFrom, Bold(Colorize(foundArg)));
+        }
+
+        return InputText;
+    }
+}
---
+++ Assets/Scripts/QuestSystem/Classes/QuestTextSimpleProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Simple Replacement", menuName = "Quest Text Processors/Single")]
+public class QuestTextSimpleProcessor : QuestTextProcessor
+{
+    public string Text;
+    
+    /// <inheritdoc />
+    public override string Process(string InputText, QuestCompletionParameters Params)
+    {
+        return InputText.Replace(Tag, Bold(Colorize(Text)));
+    }
+}
