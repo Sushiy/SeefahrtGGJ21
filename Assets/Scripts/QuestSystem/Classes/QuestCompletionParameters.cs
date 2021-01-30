e60f729378f45ce1f4ab3ab264b57d0cdf1e4fd9
Sebastian Krause <sebastian@battyRabbit.com>
2021-01-30 12:17:34 +0100

    Questsystem erweitert

---
+++ Assets/Scripts/QuestSystem/Classes/QuestCompletionParameters.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestCompletionParameters
+{
+    public GameObject PlayerGameObject { get; set; }
+    public QuestObjective CompletedObjective { get; set; }
+    
+    public Vector3 CompletionLocation { get; set; }
+    
+    public Quaternion CompletionRotation { get; set; }
+    
+    public QuestObjective NextObjective { get; set; }
+    
+}
