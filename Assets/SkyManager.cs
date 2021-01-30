760ce6a5fe9ce1db6d645437c57b9f224650e8f7
Julian Viezens <julivizi@yahoo.de>
2021-01-30 18:11:05 +0100

    Sky Shader!

--- Assets/SkyManager.cs
+++ Assets/SkyManager.cs
@@ -6,12 +6,15 @@ public class SkyManager : MonoBehaviour
 {
     public Transform sunParent;
     public float timeOfDay = 0.0f;
-    public float timeSpeed = 1.0f;
+    public float dayLengthMinutes = 3.0f;
+    private float timeSpeed;
 
+    private int shader_TimeValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeSpeed = 24.0f / dayLengthMinutes / 60.0f;
+        shader_TimeValue = Shader.PropertyToID("_TimeValue");
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class SkyManager : MonoBehaviour
         {
             timeOfDay -= 24.0f;
         }
+        Shader.SetGlobalFloat(shader_TimeValue, timeOfDay / 24.0f);
 
         float sunRotation = Mathf.Lerp(0, 360, timeOfDay / 24.0f);
         sunParent.localRotation = Quaternion.Euler(-30.0f, 0.0f, sunRotation);
