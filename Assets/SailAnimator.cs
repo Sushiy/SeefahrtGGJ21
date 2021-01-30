e70058694b3f882dd27c8aec1c03a421c30df9a4
Julian Viezens <julivizi@yahoo.de>
2021-01-30 23:53:33 +0100

    Animated Sail!
    PArticles now listen to speed!

---
+++ Assets/SailAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailAnimator : MonoBehaviour
+{
+    BoatControl controller;
+    Animator anim;
+    int sailId = Animator.StringToHash("Sail");
+    int windId = Animator.StringToHash("Wind");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInParent<BoatControl>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat(sailId, controller.ForwardVel/controller.m_maxVerticalSpeed);
+        anim.SetFloat(windId, 1.0f);
+    }
+}
---
+++ Assets/ShipParticleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipParticleController : MonoBehaviour
+{
+    BoatControl controller;
+
+    public ParticleSystem bugWellenPS_L;
+    ParticleSystem.VelocityOverLifetimeModule velocity_L;
+    public ParticleSystem bugWellenPS_R;
+    ParticleSystem.VelocityOverLifetimeModule velocity_R;
+    public ParticleSystem trail;
+    ParticleSystem.VelocityOverLifetimeModule velocity_Trail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInParent<BoatControl>();
+        velocity_L = bugWellenPS_L.velocityOverLifetime;
+        velocity_R = bugWellenPS_R.velocityOverLifetime;
+        velocity_Trail = trail.velocityOverLifetime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float normalizedSpeed = controller.ForwardVel/ controller.m_maxVerticalSpeed;
+        bugWellenPS_L.emissionRate = 200 * normalizedSpeed;
+        velocity_L.speedModifierMultiplier = normalizedSpeed;
+        bugWellenPS_R.emissionRate = 200 * normalizedSpeed;
+        velocity_R.speedModifierMultiplier = normalizedSpeed;
+
+        velocity_Trail.speedModifierMultiplier = normalizedSpeed;
+    }
+}
