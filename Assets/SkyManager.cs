beb6d76bf6ac32da60cef2fce8cb71a1be60ba2f
Julian Viezens <julivizi@yahoo.de>
2021-01-30 18:27:12 +0100

    Added Skylight!

--- Assets/SkyManager.cs
+++ Assets/SkyManager.cs
@@ -9,12 +9,15 @@ public class SkyManager : MonoBehaviour
     public float dayLengthMinutes = 3.0f;
     private float timeSpeed;
 
+    public Gradient skyGradient;
+
     private int shader_TimeValue;
     // Start is called before the first frame update
     void Start()
     {
         timeSpeed = 24.0f / dayLengthMinutes / 60.0f;
         shader_TimeValue = Shader.PropertyToID("_TimeValue");
+        
     }
 
     // Update is called once per frame
@@ -25,9 +28,11 @@ public class SkyManager : MonoBehaviour
         {
             timeOfDay -= 24.0f;
         }
-        Shader.SetGlobalFloat(shader_TimeValue, timeOfDay / 24.0f);
+        float timeOfDayNorm = timeOfDay / 24.0f;
+        Shader.SetGlobalFloat(shader_TimeValue, timeOfDayNorm);
+        RenderSettings.ambientLight = skyGradient.Evaluate(timeOfDayNorm);
 
-        float sunRotation = Mathf.Lerp(0, 360, timeOfDay / 24.0f);
+        float sunRotation = Mathf.Lerp(0, 360, timeOfDayNorm);
         sunParent.localRotation = Quaternion.Euler(-30.0f, 0.0f, sunRotation);
     }
 }
