556f3b3441376de5229a7368db01af7880a4edb3
Julian Viezens <julivizi@yahoo.de>
2021-01-30 17:34:14 +0100

    Added Sun and Sky!

--- Assets/SkyManager.cs
+++ Assets/SkyManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SkyManager : MonoBehaviour
 {
+    public Transform sunParent;
+    public float timeOfDay = 0.0f;
+    public float timeSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class SkyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeOfDay += timeSpeed * Time.deltaTime;
+        if(timeOfDay > 24.0f)
+        {
+            timeOfDay -= 24.0f;
+        }
+
+        float sunRotation = Mathf.Lerp(0, 360, timeOfDay / 24.0f);
+        sunParent.localRotation = Quaternion.Euler(-30.0f, 0.0f, sunRotation);
     }
 }
